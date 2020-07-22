e1250b9e2f9c1e2a3b3148de2078e685c077ccdf
Martin Diehl <mail@martin-diehl.net>
2020-07-22 23:21:16 +0200

    full test suite for quaternion (works)

--- src/test_rotations.f90
+++ src/test_rotations.f90
@@ -2,6 +2,8 @@ program test_rotations
   use rotations
 
   implicit none
+  integer, parameter :: N = 1100
+  real(pReal), parameter :: scatter = 1.e-2_pReal
   real(pReal), dimension(4,52) :: setOfQuaternions = &
     reshape([1.0, 0.0, 0.0, 0.0, &
              !------------------
@@ -65,14 +67,19 @@ program test_rotations
              1.0,-1.0,-1.0,-1.0],[4,52])
   
   real(pReal), dimension(:,:), allocatable :: testData
-  integer :: i,N
+  real(pReal), dimension(size(setOfQuaternions,1),size(setOfQuaternions,2)) :: noise
+  integer :: i 
   
-  N = size(setOfQuaternions,2)
   allocate(testData(4,N))
+  call random_number(testData)
   testData(:,:size(setOfQuaternions,2)) = setOfQuaternions
+  call random_number(noise)
+  testData(:,size(setOfQuaternions,2)+1:size(SetOfQuaternions,2)*2) = setOfQuaternions &
+                                                                    + (noise*2.0_pReal -1.0_pReal) * scatter
 
   do i = 1, size(testData,2)
     testData(:,i) = testData(:,i)/norm2(testData(:,i))
+    if(testData(1,i) < 0.0_pReal) testData(:,i) = -1.0_pReal * testData(:,i)
   enddo
 
   do i = 1, size(testData,2)
@@ -89,6 +96,11 @@ subroutine quaternion(qu)
   real(pReal), dimension(4) :: qu
   
   if(.not. quaternion_equal(qu,om2qu(qu2om(qu)))) stop 'om2qu/qu2om'
+  if(.not. quaternion_equal(qu,eu2qu(qu2eu(qu)))) stop 'eu2qu/qu2eu'
+  if(.not. quaternion_equal(qu,ax2qu(qu2ax(qu)))) stop 'ax2qu/qu2ax'
+  if(.not. quaternion_equal(qu,ro2qu(qu2ro(qu)))) stop 'ro2qu/qu2ro'
+  if(.not. quaternion_equal(qu,ho2qu(qu2ho(qu)))) stop 'ho2qu/qu2ho'
+  if(.not. quaternion_equal(qu,cu2qu(qu2cu(qu)))) stop 'cu2qu/qu2cu'
 
 end subroutine quaternion
 
